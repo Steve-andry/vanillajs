09d7afd86fd0530f422ee494e76241c881861da3
steven urbina <steven_urbina@hotmail.com>
2022-08-22 17:23:24 -0400

    para resolver el launch del boiler

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+  alert("hola");
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Hello World!");
 };
